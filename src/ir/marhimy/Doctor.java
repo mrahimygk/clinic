1f3e3e444736a937bc43d2d8d9feeff167e8d8e4
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 19:10:24 +0430

    m

--- src/ir/marhimy/Doctor.java
+++ src/ir/marhimy/Doctor.java
@@ -1,34 +1,29 @@
 package ir.marhimy;
 
-import java.util.UUID;
-
 public class Doctor extends Thread {
 
     public final int averageTime;
-    public final int restTime = 1;
-    public boolean isFree = true;
-    public int isFreeTime = 0;
-    final UUID id;
+    final String id;
 
-    public Doctor(int averageTime) {
+    final QueueHolder queueHolder;
+
+    public Doctor(String id, int averageTime, QueueHolder queueHolder) {
+        this.id = id;
         this.averageTime = averageTime;
-        id = UUID.randomUUID();
+        this.queueHolder = queueHolder;
     }
 
     @Override
     public void run() {
-        while (true){
+        while (true) {
             try {
-                //rest
-                System.out.println("(Doctor "+id+") : will take "+ restTime + " minutes to rest");
-                sleep(Utils.mapMinutesToCpuMillis(restTime));
-
-                //get patient
                 int sessionDuration = Distribution.calculateRandomSessionDuration(averageTime);
                 long duration = Utils.mapMinutesToCpuMillis(sessionDuration);
-                System.out.println("(Doctor "+id+") : accepted patient " + producer.get().toJson());
-                System.out.println("(Doctor "+id+") : will take "+ sessionDuration + " minutes to consult");
+                System.out.println("(Dr. " + id + "): accepting next patient");
+                final Patient patient = queueHolder.get(this);
+                System.out.println("(Dr. " + id + "): welcome patient " + patient.id+" I have " + sessionDuration + " minutes for you");
                 sleep(duration);
+                System.out.println("(Dr. " + id + "): Done consulting (patient " + patient.id + ")");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
