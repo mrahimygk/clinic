e88306ddafc5806e857b3f9d19d71f8cd8a8e396
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 19:31:33 +0430

    m

--- src/ir/marhimy/Doctor.java
+++ src/ir/marhimy/Doctor.java
@@ -21,7 +21,7 @@ public class Doctor extends Thread {
                 long duration = Utils.mapMinutesToCpuMillis(sessionDuration);
                 System.out.println("(Dr. " + id + "): accepting next patient");
                 final Patient patient = queueHolder.get(this);
-                System.out.println("(Dr. " + id + "): welcome patient " + patient.id+" I have " + sessionDuration + " minutes for you");
+                System.out.println("(Dr. " + id + "): accepting patient " + patient.id+". I have " + sessionDuration + " minutes for you");
                 sleep(duration);
                 System.out.println("(Dr. " + id + "): Done consulting (patient " + patient.id + ")");
             } catch (InterruptedException e) {
