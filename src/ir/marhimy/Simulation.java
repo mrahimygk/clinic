aba31877ec4b38b56ff4193f7698957a510fb0b7
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-10 19:31:04 +0430

    stopping after 1 minute

--- src/ir/marhimy/Simulation.java
+++ src/ir/marhimy/Simulation.java
@@ -3,6 +3,8 @@ package ir.marhimy;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class Simulation {
     public Simulation() {
 
@@ -20,5 +22,17 @@ public class Simulation {
 
         queueHolder.start();
         doctorList.forEach(Thread::start);
+
+        new Thread(() -> {
+            try {
+                sleep(1 * 60 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            queueHolder.stop();
+            doctorList.forEach(Thread::stop);
+            System.out.println("END");
+        }).start();
     }
 }
