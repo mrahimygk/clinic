2afad1966d8db72466013198c68e6db82fa942da
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-11 22:12:03 +0430

    +stats

--- src/ir/marhimy/PatientStats.java
+++ src/ir/marhimy/PatientStats.java
@@ -7,20 +7,23 @@ public class PatientStats implements Comparable<PatientStats> {
     final Integer sessionDuration;
     final Integer timeSpentInQueue;
     final Long enqueueTime;
+    final Long acceptTime;
 
-    public PatientStats(Integer queuePosition, String selectedDoctor, Integer sessionDuration, Integer timeSpentInQueue, Long enqueueTime) {
+    public PatientStats(Integer queuePosition, String selectedDoctor, Integer sessionDuration, Integer timeSpentInQueue, Long enqueueTime, Long acceptTime) {
         this.queuePosition = queuePosition;
         this.selectedDoctor = selectedDoctor;
         this.sessionDuration = sessionDuration;
         this.timeSpentInQueue = timeSpentInQueue;
         this.enqueueTime = enqueueTime;
+        this.acceptTime = acceptTime;
     }
 
-    public PatientStats copyWith(Integer queuePosition, String selectedDoctor, Integer sessionDuration, Integer timeSpentInQueue) {
+    public PatientStats copyWith(Integer queuePosition, String selectedDoctor, Integer sessionDuration, Integer timeSpentInQueue, Long acceptTime) {
         Integer outQueuePosition;
         String outSelectedDoctor;
         Integer outSessionDuration;
         Integer outTimeSpentInQueue;
+        Long outAcceptTime;
         if (queuePosition == null) outQueuePosition = this.queuePosition;
         else outQueuePosition = queuePosition;
         if (selectedDoctor == null) outSelectedDoctor = this.selectedDoctor;
@@ -29,12 +32,15 @@ public class PatientStats implements Comparable<PatientStats> {
         else outSessionDuration = sessionDuration;
         if (timeSpentInQueue == null) outTimeSpentInQueue = this.timeSpentInQueue;
         else outTimeSpentInQueue = timeSpentInQueue;
+        if (acceptTime == null) outAcceptTime = this.acceptTime;
+        else outAcceptTime = acceptTime;
         return new PatientStats(
                 outQueuePosition,
                 outSelectedDoctor,
                 outSessionDuration,
                 outTimeSpentInQueue,
-                enqueueTime);
+                enqueueTime,
+                outAcceptTime);
     }
 
     @Override
